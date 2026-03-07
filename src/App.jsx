24205a666571183e925c4eb67ccb9100d9f34a5e
tipicoUEFN <128237382+tipicoUEFN@users.noreply.github.com>
2026-03-07 21:47:49 +0100

    Force manual scroll restoration on refresh

--- src/App.jsx
+++ src/App.jsx
@@ -17,6 +17,12 @@ function ScrollToTop() {
   const { pathname } = useLocation()
 
   useEffect(() => {
+    if ('scrollRestoration' in window.history) {
+      window.history.scrollRestoration = 'manual'
+    }
+  }, [])
+
+  useEffect(() => {
     window.scrollTo({ top: 0, left: 0, behavior: 'auto' })
   }, [pathname])
 
